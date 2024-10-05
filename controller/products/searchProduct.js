37f4b3c7e505accde7567728c70be2304f701dfc
arsalanahmad123 <arsalanahmad15092003@gmail.com>
2024-10-05 22:02:26 +0500

    3rd push

--- controller/products/searchProduct.js
+++ controller/products/searchProduct.js
@@ -1,42 +1,28 @@
-const productModel = require("../../models/productModel");
+const productModel = require('../../models/productModel');
 
 const searchProductController = async (req, res) => {
-  try {
-    // Get the search query from the request
-    const query = req.query.q;
-    console.log("query :: ", query);
+    try {
+        const query = req.query.q;
+        console.log('query :: ', query);
 
-    // Create a regex pattern for case-insensitive and global search
-    const regex = new RegExp(query, "i"); // 'i' for case insensitive
+        const regex = new RegExp(query, 'i');
 
-    // Search the product model for matches in productName or category fields
-    const product = await productModel.find({
-      $or: [
-        { title: regex },
-        { category: regex },
-        { customerType: regex },
-        { viscosity: regex },
-        { recommendedBy: regex },
-        { articleNumber: regex },
-        { approvedFor: regex },
-      ],
-    });
+        const products = await productModel.find({ title: regex });
 
-    // Send a successful response with the search results
-    res.json({
-      message: "FETCHED THE SEARCH PRODUCTS",
-      error: false,
-      success: true,
-      data: product,
-    });
-  } catch (error) {
-    // Handle any errors and send an error response
-    res.status(400).json({
-      message: error.message || error,
-      error: true,
-      success: false,
-    });
-  }
+        res.json({
+            message: 'FETCHED THE SEARCH PRODUCTS',
+            error: false,
+            success: true,
+            data: products,
+        });
+    } catch (error) {
+        // Handle any errors and send an error response
+        res.status(400).json({
+            message: error.message || error,
+            error: true,
+            success: false,
+        });
+    }
 };
 
 module.exports = searchProductController;
